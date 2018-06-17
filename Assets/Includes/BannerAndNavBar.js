3de94c3b3e903ff73d6bbe77ddaa48e43e98be3e
JamesHollowayCurrie <enzod43@gmail.com>
2018-06-17 12:05:45 -0600

    Floating Nav and Light/Dark Toggle

--- Assets/Includes/BannerAndNavBar.js
+++ Assets/Includes/BannerAndNavBar.js
@@ -1,4 +1,40 @@
 
+var lightProfile;
+
+function toggleLightDark()
+{
+	var items = document.getElementsByClassName("portfolio");
+	var headings = document.getElementsByClassName("plainHeading");
+	//alert(lightProfile + " " + items.length);
+	if(lightProfile)
+	{
+		for(var i = 0; i < items.length; i++)
+		{
+			items[i].style.color = "#000";
+			if(items[i].nodeName == "IMG" || items[i].nodeName == "VIDEO")
+				items[i].style.boxShadow = "0 0 10px grey";
+		}
+		for(var i = 0; i < headings.length; i++)
+			headings[i].style.color = "black";
+		document.getElementById("content").style.background = "white";
+		document.getElementById("toggle").style.boxShadow = "0 0 4px 8px black";
+	}
+	else
+	{
+		for(var i = 0; i < items.length; i++)
+		{
+			items[i].style.color = "#ccc";
+			if(items[i].nodeName == "IMG" || items[i].nodeName == "VIDEO")
+				items[i].style.boxShadow = "0 0 0 grey";
+		}
+		for(var i = 0; i < headings.length; i++)
+			headings[i].style.color = "#ccc";
+		document.getElementById("content").style.background = "#222";
+		document.getElementById("toggle").style.boxShadow = "0 0 4px 8px #0f0";
+	}
+	//alert(document.getElementById("content").nodeName);
+}
+
 document.write('<div id="banner">');
 document.write('<h1>');
 document.write('<a id="logo" href="index.html"><img src="Assets/Media/Images/logo.png" alt="JHC Media" title="JHC Media" style="width: 328px; height: 69px;"/></a></h1>');
@@ -12,11 +48,14 @@ else
 	document.write('<li id="Home"><a href="index.html" class ="">Games</a></li>');	
 
 if(document.title == "JHC Media - About")
-	document.write('<li><a href="about.html" class ="current">About</a></li>');
+	document.write('<li><a href="about.html" class="current">About</a></li>');
 else
 	document.write('<li><a href="about.html" class ="">About</a></li>');
 
 document.write('<li><a href="Holloway-Currie,_James_Resume.pdf" class ="">Resume</a></li>');
 
 document.write('<li><a href="http://www.linkedin.com/pub/james-holloway-currie/89/7b9/760?domainCountryName=&csrfToken=ajax%3A4999612905356199504" class ="" style="margin: 0; padding: 4px 16px;"><img src="Assets/Media/Images/in.png" alt="LinkedIn" title="LinkedIn" style="width: 30px; height: 30px; vertical-align: top; padding-top: 2px;"/></a></li>');
+
+document.write('<li><a onclick="lightProfile = !lightProfile; toggleLightDark();"><img id="toggle"  src="Assets/Media/Images/thunderbowl-icon.png" alt="Toggle Light/Dark view" title="Toggle Light/Dark View" style="width: 14px; height 14px; vertical-align: top; margin-top: 11px; border-radius: 32px; box-shadow: 0 0 4px 8px #0f0;"/></a></li>');
+
 document.write('</ul>');
--- Assets/Includes/CommonMetadata.js
+++ Assets/Includes/CommonMetadata.js
@@ -10,4 +10,4 @@ document.write('<meta property="og:image" content="http://www.jhcmedia.ca/Assets
 document.write('<meta property="og:url" content="http://www.jhcmedia.ca">');
 document.write('<meta property="og:description" content="JHC Media is the primary portfolio site for James Holloway-Currie. It features a number of game industry projects made using primarily the Visual C# and Visual C++ programming languages.">');
 document.write('<link rel="icon" href="favicon.ico"/>');
-document.write('<link rel="stylesheet" type="text/css" href="styles.css" media="all"/>');
+document.write('<link id="pagestyle" rel="stylesheet" type="text/css" href="styles.css" media="all"/>');
