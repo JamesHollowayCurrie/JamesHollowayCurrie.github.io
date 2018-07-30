8c4f226f9a8443f8fa5423858fafb975782e9176
JamesHollowayCurrie <enzod43@gmail.com>
2018-07-30 09:55:13 -0600

    update

--- Assets/Includes/BannerAndNavBar.js
+++ Assets/Includes/BannerAndNavBar.js
@@ -6,31 +6,45 @@ function toggleLightDark()
 	var items = document.getElementsByClassName("portfolio");
 	var headings = document.getElementsByClassName("plainHeading");
 	//alert(lightProfile + " " + items.length);
+	var shortTrans = "0.5s ease all";
+	var longTrans = "2.0s ease all";
 	if(lightProfile)
 	{
 		for(var i = 0; i < items.length; i++)
 		{
 			items[i].style.color = "#000";
+			items[i].style.transition = longTrans;
 			if(items[i].nodeName == "IMG" || items[i].nodeName == "VIDEO")
 				items[i].style.boxShadow = "0 0 10px grey";
 		}
 		for(var i = 0; i < headings.length; i++)
+		{
 			headings[i].style.color = "black";
+			headings[i].style.transition = longTrans;
+		}
 		document.getElementById("content").style.background = "white";
-		document.getElementById("toggle").style.boxShadow = "0 0 4px 8px black";
+		document.getElementById("content").style.transition = longTrans;
+		document.getElementById("toggle").style.boxShadow = "0 0 6px 12px black";
+		document.getElementById("toggle").style.transition = shortTrans;
 	}
 	else
 	{
 		for(var i = 0; i < items.length; i++)
 		{
 			items[i].style.color = "#ccc";
+			items[i].style.transition = longTrans;
 			if(items[i].nodeName == "IMG" || items[i].nodeName == "VIDEO")
 				items[i].style.boxShadow = "0 0 0 grey";
 		}
 		for(var i = 0; i < headings.length; i++)
+		{
 			headings[i].style.color = "#ccc";
+			headings[i].style.transition = longTrans;
+		}
 		document.getElementById("content").style.background = "#222";
-		document.getElementById("toggle").style.boxShadow = "0 0 4px 8px #0f0";
+		document.getElementById("content").style.transition = longTrans;
+		document.getElementById("toggle").style.boxShadow = "0 0 6px 12px #0f0";
+		document.getElementById("toggle").style.transition = shortTrans;
 	}
 	//alert(document.getElementById("content").nodeName);
 }
@@ -58,6 +72,6 @@ document.write('<li><a href="http://www.linkedin.com/pub/james-holloway-currie/8
 
 document.write('<li><a href="https://twitter.com/MediaJhc" class ="" style="margin: 0; padding: 4px 12px;"><img src="Assets/Media/Images/twitter.png" alt="Twitter@MediaJHC" title="Twitter@MediaJHC" style="width: 28px; height: 28px; vertical-align: top; padding-top: 3px;"/></a></li>');
 
-document.write('<li><a onclick="lightProfile = !lightProfile; toggleLightDark();"><img id="toggle"  src="Assets/Media/Images/thunderbowl-icon.png" alt="Toggle Light/Dark view" title="Toggle Light/Dark View" style="width: 14px; height 14px; vertical-align: top; margin-top: 11px; border-radius: 32px; box-shadow: 0 0 4px 8px #0f0;"/></a></li>');
+document.write('<li title="Toggle Light/Dark View"><a onclick="lightProfile = !lightProfile; toggleLightDark();"><img id="toggle"  src="Assets/Media/Images/lightToggle.png" style="width: 1px; height 1px; vertical-align: top; margin: 0 5px; margin-top: 17px; border-radius: 32px; box-shadow: 0 0 6px 12px #0f0;"/></a></li>');
 
 document.write('</ul>');
